e803ab06a2545f9dd5af45bcb9bd41dbc4e8af2f
niran-mutakani <niranmutakni@example.com>
2026-05-10 12:45:16 +0000

    table name modified in the model

--- models/finance_controls_results.sql
+++ models/finance_controls_results.sql
@@ -12,6 +12,6 @@ select 1 as CONTROL_ID, src.VALUATION_DATE as VALUATION_DATE, 'TB_VS_FPSL' CONTR
 case when DIFFERENCE_VALUE=0 then 'Pass' else 'FAIL' end PASS_FAIL_FLAG, 0 as REMARKS, CURRENT_TIMESTAMP() CREATE_TS,CURRENT_TIMESTAMP() as UPDATE_TS, CURRENT_USER() as CREATE_USER,CURRENT_USER() as UPDATE_USER
 from 
 (select department SOURCE_MEASURE_NAME,VALUATION_DATE, sum(salary) as SOURCE_VALUE from trail_balance_table group by department,VALUATION_DATE) src
-inner join (select department TARGET_MEASURE_NAME, VALUATION_DATE,sum(salary) as TARGET_VALUE from trail_balance_table group by department,VALUATION_DATE) tar 
+inner join (select department TARGET_MEASURE_NAME, VALUATION_DATE,sum(salary) as TARGET_VALUE from FPSL_TABLE group by department,VALUATION_DATE) tar 
 on src.SOURCE_MEASURE_NAME  = tar.TARGET_MEASURE_NAME
 and src.VALUATION_DATE= tar.VALUATION_DATE
